Plotted Hessian, need bugfix

Code/testCircle.py
@@ -3,6 +3,12 @@
 # License       :
 # Description   :
 
+### TO DOs:
+# --- Add p movements
+# --- Store cost values on p trajectory
+# --- Plot against filtered and theta value
+# --- Fix error on second component of Hessian
+
 ### MODULES
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,7 +23,7 @@ class Simulation:
     # Simulation parameters
     self.t0 = 0.0                                                 # Initial time
     self.tF = 100.0                                               # Final time
-    self.dt = 0.0001                                              # Simulation time step
+    self.dt = 0.01                                              # Simulation time step
     self.steps = int((self.tF-self.t0)/self.dt) + 1               # Number of simulation steps
     self.time_span = np.linspace(self.t0, self.tF, self.steps)    # Time vector
 
@@ -32,7 +38,7 @@ class Simulation:
     # Algorithm quantities
     self.y0 = self.distanceFromCircularWall(self.p)   # Initial filtered cost
     self.y1 = self.gradient(self.p) 
-    self.y2 = np.zeros((2,))
+    self.y2 = 2.0*np.eye(2)
 
     # Plotting quantities
     self.p_traj = np.zeros((self.steps, 2))
@@ -50,8 +56,14 @@ class Simulation:
     self.gradJ_traj = np.zeros((self.steps, 2))
     self.gradJ_traj[0, :] = self.y1.copy() 
 
+    self.hessJ_traj = np.zeros((self.steps, 2, 2))
+    self.hessJ_traj[0, :] = self.y2.copy() 
+
     self.y1_traj = np.zeros((self.steps, 2))
     self.y1_traj[0, :] = self.y1.copy() 
+
+    self.y2_traj = np.zeros((self.steps, 2, 2))
+    self.y2_traj[0, :, :] = self.y2.copy() 
 
   def distanceFromCircularWall(self, p):
     norm = np.linalg.norm(p)
@@ -59,7 +71,10 @@ class Simulation:
     return np.linalg.norm(p) ** 2
 
   def gradient(self, p):
-    return 2*p.copy() 
+    return 2.0*p.copy() 
+
+  def hessian(self, p):
+    return 2.0 * np.eye(2)
 
   def run(self):
     for ii in range(self.steps - 1):
@@ -71,10 +86,16 @@ class Simulation:
         np.sin(self.w/2.0 * t)
       ])
 
-      # Demodulation signal
+      # Demodulation signal (Gradient)
       M = np.array([
         2.0/self.a * np.sin(self.w * t),
         2.0/self.a * np.sin(self.w/2.0 * t)
+      ])
+
+      # Demodulation signal (Hessian)
+      N = np.array([
+        [16.0 / self.a ** 2.0 * (np.sin(self.w * t)**2.0 - 1.0/2.0), 4.0 / (self.a * self.a) * np.sin(self.w * t) * np.sin(self.w / 2.0 * t)], 
+        [4.0 / (self.a * self.a) * np.sin(self.w * t) * np.sin(self.w / 2.0 * t), 16.0 / self.a ** 2.0 * (np.sin(self.w/2.0 * t)**2.0 - 1.0/2.0)] 
       ])
 
       # Update position
@@ -88,13 +109,16 @@ class Simulation:
       diff = self.J - self.y0
       self.y0 = self.y0 + self.h * diff * self.dt
       self.y1 = self.y1 + self.l * ( -self.y1 + diff * M ) * self.dt
+      self.y2 = self.y2 + self.l * ( -self.y2 + diff * N ) * self.dt 
       
       # Update plots
       self.theta_traj[ii+1, :] = self.theta.copy()
       self.J_traj[ii+1] = self.J
       self.gradJ_traj[ii+1] = self.gradient(self.theta)
+      self.hessJ_traj[ii+1] = self.hessian(self.theta)
       self.y0_traj[ii+1] = self.y0
       self.y1_traj[ii+1] = self.y1.copy()
+      self.y2_traj[ii+1] = self.y2.copy()
 
   def plot(self):
     # State
@@ -113,11 +137,25 @@ class Simulation:
     ax.legend()
     ax.set_title("Distance")
 
-    fig, ax = plt.subplots()
-    plt.plot(self.time_span, self.gradJ_traj[:, 0], label="Real value")
-    plt.plot(self.time_span, self.y1_traj[:, 0], label="Filtered value")
-    ax.legend()
-    ax.set_title("Gradient")
+    fig, ax = plt.subplots(1, 2, sharey='row')
+    ax[0].plot(self.time_span, self.gradJ_traj[:, 0], label="Real value")
+    ax[0].plot(self.time_span, self.y1_traj[:, 0], label="Filtered value")
+    ax[1].plot(self.time_span, self.gradJ_traj[:, 1], label="Real value")
+    ax[1].plot(self.time_span, self.y1_traj[:, 1], label="Filtered value")
+    ax[0].legend()
+    ax[1].legend()
+    ax[0].set_title("Gradient 1")
+    ax[1].set_title("Gradient 2")
+
+    fig, ax = plt.subplots(2, 2)
+    ax[0, 0].plot(self.time_span, self.hessJ_traj[:, 0, 0], label="Real value")
+    ax[0, 0].plot(self.time_span, self.y2_traj[:, 0, 0], label="Filtered value")
+    ax[0, 1].plot(self.time_span, self.hessJ_traj[:, 0, 1], label="Real value")
+    ax[0, 1].plot(self.time_span, self.y2_traj[:, 0, 1], label="Filtered value")
+    ax[1, 0].plot(self.time_span, self.hessJ_traj[:, 1, 0], label="Real value")
+    ax[1, 0].plot(self.time_span, self.y2_traj[:, 1, 0], label="Filtered value")
+    ax[1, 1].plot(self.time_span, self.hessJ_traj[:, 1, 1], label="Real value")
+    ax[1, 1].plot(self.time_span, self.y2_traj[:, 1, 1], label="Filtered value")
 
     plt.show()
     plt.close()